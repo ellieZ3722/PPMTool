d3dabcbca3a2c82d38fece7cd95f7e374a88a512
qzhang7272 <qzhang72@uchicago.edu>
2020-02-18 22:17:53 -0600

    first origin

---
+++ PPM_project/src/test/java/io/elliez3722/ppmtool/PpmtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package io.elliez3722.ppmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmtoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
